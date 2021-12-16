a3e124ed6043eb730629388a1934286239390fc7
Kennedy Rose <kennedyianrose@gmail.com>
2021-12-16 15:50:40 -0600

    No prerendered pages

--- pages/[...path].js
+++ pages/[...path].js
@@ -1,22 +1,22 @@
 import DynamicPath, { getStaticProps } from './index'
-import pageList from '../helpers/ui-page-list.json'
+// import pageList from '../helpers/ui-page-list.json'
 
 export default DynamicPath
 
 export { getStaticProps }
 
 export async function getStaticPaths() {
-	const paths = []
-	for(let link of pageList){
-		const path = link.replace(`/`, ``).split(`/`)
-		if(!path.length || !path[0]) continue
-		paths.push({
-			params: { path }
-		})
-	}
+	// const paths = []
+	// for(let link of pageList){
+	// 	const path = link.replace(`/`, ``).split(`/`)
+	// 	if(!path.length || !path[0]) continue
+	// 	paths.push({
+	// 		params: { path }
+	// 	})
+	// }
 
 	return {
-	  paths: paths,
+	  paths: [],
 	  fallback: `blocking`,
 	}
  }
