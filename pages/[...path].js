31ae44d2445bee63d70fc75918b5d27355a2dcf6
Kennedy Rose <kennedyianrose@gmail.com>
2021-12-18 14:07:48 -0600

    Static pages

--- pages/[...path].js
+++ pages/[...path].js
@@ -1,22 +1,24 @@
 import DynamicPath, { getStaticProps } from './index'
-// import pageList from '../helpers/polymorph/page-list.json'
+import pageList from '../helpers/polymorph/page-list.json'
 
 export default DynamicPath
 
 export { getStaticProps }
 
 export async function getStaticPaths() {
-	// const paths = []
-	// for(let link of pageList){
-	// 	const path = link.replace(`/`, ``).split(`/`)
-	// 	if(!path.length || !path[0]) continue
-	// 	paths.push({
-	// 		params: { path }
-	// 	})
-	// }
+	const paths = []
+
+	// Add all pages
+	for(let link of pageList){
+		const path = link.replace(`/`, ``).split(`/`)
+		if(!path.length || !path[0]) continue
+		paths.push({
+			params: { path }
+		})
+	}
 
 	return {
-	  paths: [],
+	  paths,
 	  fallback: `blocking`,
 	}
  }
