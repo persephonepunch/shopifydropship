68c60885d9c56b7e268391ddc7ed11433b900f9c
Kennedy Rose <kennedyianrose@gmail.com>
2022-01-10 16:25:34 -0600

    Defaults

--- design-sync/config.js
+++ design-sync/config.js
@@ -1,11 +1,11 @@
 const config = {
-  // site: `https://sl4.webflow.io/`,
+  // site: `https://your-site.webflow.io/`,
   removeBranding: true,
   clientRouting: true,
   optimizeImages: true,
   optimizeJsLoading: true,
-  staticPageLimit: 0,
-  revalidate: 10,
+  staticPageLimit: 200,
+  revalidate: false,
 }
 
 module.exports = config
