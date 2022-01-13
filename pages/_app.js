98ad160c9693570c3c2962e56126473634fc3213
Kennedy Rose <kennedyianrose@gmail.com>
2022-01-13 16:44:07 -0600

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as ga from '../helpers/google-analytics'
 import '../.design-sync/styles.css'
+
+const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
 
 function App(props) {
   const { Component, pageProps } = props
+  const router = useRouter()
+
+  useEffect(() => {
+    if(!gaId) return
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <Component {...pageProps} />
