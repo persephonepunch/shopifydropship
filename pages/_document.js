5ccd8e961c85df5bfc3bb85fdeb63dec54c96915
Kennedy Rose <kennedyianrose@gmail.com>
2022-02-14 07:27:10 -0600

    Removed GR script

--- pages/_document.js
+++ pages/_document.js
@@ -37,7 +37,6 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
           <script src='/design-sync.js' defer></script>
-          <script src="https://gumroad.com/js/gumroad.js" defer></script>
         </body>
       </Html>
     )
